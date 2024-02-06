1f771ea43c38a6706696f9d28ad02a084bf21397
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-02-06 19:23:27 +0200

    updated

--- script.js
+++ script.js
@@ -1,42 +1,42 @@
 // task 1
 
-var age = 16;
+let age = 16;
 console.log("Мені  " + age + " років.");
 
 // task 2
 
-var name = "Dima";
+let name = "Dima";
 console.log("I`m " + name);
 
 // task 3 
 
-var isStudent = true;
+let isStudent = true;
 console.log("Student? " + isStudent);
 
 // task 4
 
-var myString = "Життя - це ігра, і тільки від тебе залежить, на яку кінцівку ти її закінчиш!";
+let myString = "Життя - це ігра, і тільки від тебе залежить, на яку кінцівку ти її закінчиш!";
 console.log("Моя любима цитата: " + myString);
 
 // task 5
 
-var myNumber =  8;
+let myNumber =  8;
 myNumber+=10;
 console.log("myNumber: " + myNumber)
 
 // task 6
 
-var myNull = null;
+let myNull = null;
 console.log("myNull: " + myNull)
 
 // task 7
 
-var userName = prompt("Будь ласка, введіть своє ім'я");
+const userName = prompt("Будь ласка, введіть своє ім'я");
 alert("Hi " + userName)
 
 // task 8
 
-var conone = confirm("Натисніть ОК, якщо ви погоджуєтесь: ");
+const conone = confirm("Натисніть ОК, якщо ви погоджуєтесь: ");
 if (conone){
     alert ("Дякую за підтвердження!");
 }
@@ -46,7 +46,7 @@ else{
 
 // task 9
 alert("УВАГА! Дія небезпечна!");
-var contwo = confirm("Натисніть ОК, якщо ви погоджуєтесь: ");
+const contwo = confirm("Натисніть ОК, якщо ви погоджуєтесь: ");
 if (contwo){
     alert ("Дякую за підтвердження!");
 }
