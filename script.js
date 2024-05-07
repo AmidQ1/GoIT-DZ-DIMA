a250b739c2ecf7154e4b7eecd47591b1c6d2ac2e
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-05-07 19:29:44 +0300

    fix

--- script.js
+++ script.js
@@ -7,27 +7,27 @@ message()
 
 // task 2
 const randomMessage = prompt("число від 1 до 100");
-const randomNumber = () => Math.floor(Math.random() * 100) + 1;
-if (randomMessage == randomNumber()) {
+const randomNumber = Math.floor(Math.random() * 100) + 1;
+if (randomMessage == randomNumber) {
     alert("You win!");
 } else {
     alert("You lose!");
 }
-console.log("Your: ", randomMessage, "Random: ", randomNumber());
+console.log("Your: ", randomMessage, "Random: ", randomNumber);
 
 // task 3
-const howClick = 0;
+// const howClick = 0;
 
-const click = ('click', () => {
-    clickCount++;
-    alert(`Кількість кліків: ${clickCount}`);
-});
+// const click = ('click', () => {
+//     clickCount++;
+//     alert(`Кількість кліків: ${clickCount}`);
+// });
 
-// I don`t know how to do it
+// Sorry, I don`t see that.
 
 //  task 4
 
-const thereALotOfText =(arr, callback) =>{
+const applyCallbackToEachElement =(arr, callback) =>{
     const result = []; 
     for (let i = 0; i < arr.length; i++) {
         result.push(callback(arr[i])); 
@@ -37,7 +37,7 @@ const thereALotOfText =(arr, callback) =>{
 const arr = [1,2,3,4,5];
 const sqrtCallback = num => num * num;
 
-const result = thereALotOfText(arr, sqrtCallback);
+const result = applyCallbackToEachElement(arr, sqrtCallback);
 console.log(result);
 
 // task 5
