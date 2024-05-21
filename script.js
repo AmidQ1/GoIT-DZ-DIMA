1b5a38b590ca3022c3e8d04fe57e547cf0f6e4a3
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-05-21 18:41:37 +0300

    hw(Імперативний і декларативний код. Чисті функції)

---
+++ script.js
@@ -0,0 +1,7 @@
+// task 1
+const getUserNames = users => {
+    
+};
+  
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
