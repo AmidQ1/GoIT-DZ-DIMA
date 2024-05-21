0f09b8acc87bafe896063904f4514ebc3e41be43
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-05-21 19:04:13 +0300

    add

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 // task 1
 const getUserNames = users => {
-    
+    return users.map(user => user[name], user[surname])
 };
-  
+
+const users = [
+    { name: 'Moore', surname: 'Hensley' },
+    { name: 'Sharlene', surname: 'Bush' },
+    { name: 'Ross', surname: 'Vazquez' },
+    { name: 'Elma', surname: 'Head' },
+    { name: 'Carey', surname: 'Barr' },
+    { name: 'Blackburn', surname: 'Dotson' },
+    { name: 'Sheree', surname: 'Anthony' },
+]
+
 console.log(getUserNames(users));
 // [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
