dc8234f87eb97e1a3eaabed19542fbb7e082653f
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-06-04 17:43:46 +0300

    add

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-console.log(Account.prototype.getInfo); // function
-
+// task 1
+function Account({login, email}){
+    this.login = login;
+    this.email = email;
+}
+Account.prototype.getInfo = function(){
+    console.log(`Login: ${this.login}, Email: ${this.email}`); // function
+}
 const mango = new Account({
   login: 'Mangozedog',
   email: 'mango@dog.woof',
 });
 
-mango.getInfo(login, email); // Login: Mangozedog, Email: mango@dog.woof
+mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
 
 const poly = new Account({
   login: 'Poly',
@@ -13,3 +19,22 @@ const poly = new Account({
 });
 
 poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+// task 2
+
+
+const mango1 = new User({
+    name: 'Mango',
+    age: 2,
+    followers: 20,
+});
+  
+mango1.getInfo(); // User Mango is 2 years old and has 20 followers
+  
+const poly1 = new User({
+    name: 'Poly',
+    age: 3,
+    followers: 17,
+});
+  
+poly1.getInfo(); // User Poly is 3 years old and has 17 followers
