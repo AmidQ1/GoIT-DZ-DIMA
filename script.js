74f41f2570aa6ce3005f3791e9893b1e74d2b972
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-06-25 19:14:18 +0300

    add

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const img = document.getElementById('image').src = './images/new-image.jpg';
 
 // task 3
 
-const imgLink =document.getElementById('link').href = 'https://store.steampowered.com/?l=ukrain';
+const imgLink = document.getElementById('link').href = 'https://store.steampowered.com/?l=ukrain';
 const imgAlt = document.getElementById('image').alt = 'steam';
 
 // task 4
