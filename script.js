0f54448b8bb2bd16c0327f8e9f3a169b5aac1c03
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-06-25 19:11:36 +0300

    add

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 // task 1
 
-document.getElementById('changeButton').addEventListener('click', () => {
+const chBut = document.getElementById('changeButton').addEventListener('click', () => {
     const textFieldValue = document.getElementById('textField').value;
     document.getElementById('changeButton').textContent = textFieldValue;
 });
 
 // task 2
 
-document.getElementById('image').src = './images/new-image.jpg';
+const img = document.getElementById('image').src = './images/new-image.jpg';
 
 // task 3
 
-document.getElementById('link').href = 'https://store.steampowered.com/?l=ukrain';
-document.getElementById('image').alt = 'steam';
+const imgLink =document.getElementById('link').href = 'https://store.steampowered.com/?l=ukrain';
+const imgAlt = document.getElementById('image').alt = 'steam';
 
 // task 4
 
 const firstListItem = document.querySelector('ul li');
-firstListItem.textContent = 'Новий текст першого елемента';
+const changeTextFirst = firstListItem.textContent = 'Новий текст першого елемента';
