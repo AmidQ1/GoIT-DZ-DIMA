aede12f235749d8f0ce4e9b6dd102597ae9a06e3
Dima Luzhny <117902450+AmidQ1@users.noreply.github.com>
2024-09-08 13:10:26 +0300

    add dz 21

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
-console.log("aaaaa");
+const images = document.querySelectorAll('.image');
+const fullImageContainer = document.querySelector('.full-image-container');
+const fullImage = document.querySelector('.full-image');
+let currentIndex = 0;
+
+function showImage(index) {
+  const src = images[index].src;
+  fullImage.src = src;
+  fullImageContainer.style.display = 'flex';
+}
+
+function closeImage() {
+  fullImageContainer.style.display = 'none';
+}
+
+function handleKeyPress(event) {
+  if (event.key === 'ArrowRight') {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+  } else if (event.key === 'ArrowLeft') {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+  } else if (event.key === 'Escape') {
+    closeImage();
+  }
+}
+
+images.forEach((img, index) => {
+  img.addEventListener('click', () => {
+    currentIndex = index;
+    showImage(index);
+  });
+});
+
+fullImageContainer.addEventListener('click', closeImage);
+document.addEventListener('keydown', handleKeyPress);
+
+// 
+
+const boxesContainer = document.getElementById('boxes');
+const renderButton = document.getElementById('render-button');
+const destroyButton = document.getElementById('destroy-button');
+const amountInput = document.getElementById('amount-input');
+
+function getRandomColor() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
+}
+
+function createBoxes(amount) {
+  const boxSizeStart = 30;
+  const boxSizeIncrement = 10;
+
+  let boxSize = boxSizeStart;
+
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+    box.style.backgroundColor = getRandomColor();
+    boxesContainer.appendChild(box);
+
+    boxSize += boxSizeIncrement;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
+}
+
+renderButton.addEventListener('click', () => {
+  const amount = Number(amountInput.value);
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
